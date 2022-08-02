990af68c4989b28d54cf5bad78acb28cf17e6b96
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-08-02 18:09:24 +0300

    end tasks, hw03 done

--- models/contacts.js
+++ models/contacts.js
@@ -1,67 +1,25 @@
-const fs = require("fs/promises");
-const path = require("path");
-const ObjectID = require("bson-objectid");
+const { Schema, model } = require("mongoose");
 
-const contactsPath = path.join(__dirname, "contacts.json");
+const contactSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
 
-const updateContacts = async (contacts) => {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-};
+const Contact = model("contact", contactSchema);
 
-const listContacts = async () => {
-  const result = await fs.readFile(contactsPath);
-  return JSON.parse(result);
-};
-
-const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const result = await contacts.find((item) => item.id === contactId);
-  if (!result) {
-    return null;
-  }
-  return result;
-};
-
-const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const idx = contacts.findIndex((item) => item.id === contactId);
-  if (idx === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(idx, 1);
-  await updateContacts(contacts);
-
-  return result;
-};
-
-const addContact = async ({ name, email, phone }) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: ObjectID(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await updateContacts(contacts);
-  return newContact;
-};
-
-const updateContact = async (contactId, body) => {
-  contacts = await listContacts();
-  const idx = contacts.findIndex((item) => item.id === contactId);
-  if (idx === -1) {
-    return null;
-  }
-  contacts[idx] = { ...body, contactId };
-  await updateContacts(contacts);
-  return contacts[idx];
-};
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = Contact;
