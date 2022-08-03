9424640d18346c55b31d21275c5cf052d0ba1cb8
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-08-03 21:52:42 +0300

    hw06 done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,12 +6,15 @@ const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
 const Jimp = require("jimp");
+const { nanoid } = require("nanoid");
 require("dotenv").config();
+
+const { SECRET_KEY } = process.env;
 
 const User = require("../../models/user");
 
 const router = express.Router();
-const createError = require("../../helpers/createError");
+const { createError, sendMail } = require("../../helpers");
 const { authorize, upload } = require("../../middlewares");
 
 const emailRegexp = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
@@ -27,7 +30,9 @@ const userLoginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
-const { SECRET_KEY } = process.env;
+const verifyEmailSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+});
 
 //avatarPath
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
@@ -47,12 +52,25 @@ router.post("/register", async (req, res, next) => {
     const hashPassword = await bcrypt.hash(password, 10);
     //add gravatar
     const avatarURL = gravatar.url(email);
+    //add verification Token
+    const verificationToken = nanoid();
+
     const result = await User.create({
       email,
       password: hashPassword,
       name,
       avatarURL,
+      verificationToken,
     });
+
+    //verification message
+    const mail = {
+      to: email,
+      subject: "Підтвердження реєстраціїї на сервері",
+      html: `<a target="_blank" href="http://localhost:3000/api/auth/${verificationToken}">Натисніть для підтвердження реєстрації</a>`,
+    };
+    await sendMail(mail);
+
     res.status(201).json({
       name: result.name,
       email: result.email,
@@ -61,8 +79,58 @@ router.post("/register", async (req, res, next) => {
     next(error);
   }
 });
-// signin
 
+//verification endpoint
+router.get("/verify/:verificationToken", async (req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
+    if (!user) {
+      throw createError(404);
+    }
+
+    await User.findByIdAndUpdate(user._id, {
+      verificationToken: "",
+      verify: true,
+    });
+    res.json({
+      message: "Verification successful",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/verify", async (req, res, next) => {
+  try {
+    const { error } = verifyEmailSchema.validate(req.body);
+    if (error) {
+      throw createError(400);
+    }
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw createError(404);
+    }
+    if (user.verify) {
+      throw createError(400, "Verification has already been passed");
+    }
+
+    const mail = {
+      to: email,
+      subject: "Підтвердження реєстраціїї на сервері",
+      html: `<a target="_blank" href="http://localhost:3000/api/users/${user.verificationToken}">Натисніть для підтвердження реєстрації</a>`,
+    };
+    await sendMail(mail);
+    res.json({
+      message: "Verification email sent",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// signin
 router.post("/login", async (req, res, next) => {
   try {
     const { error } = userLoginSchema.validate(req.body);
@@ -73,6 +141,9 @@ router.post("/login", async (req, res, next) => {
     const user = await User.findOne({ email });
     if (!user) {
       throw createError(401, "Email wrong");
+    }
+    if (!user.verify) {
+      throw createError(401, "Email not verified");
     }
 
     const passwordCompare = await bcrypt.compare(password, user.password);
